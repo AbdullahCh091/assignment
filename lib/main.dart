b014b6e6e497e3ed859bd44df03ff7778a2544cb
Abdullah <zindamujahid091@gmail.com>
2022-04-05 11:21:20 -0700

    assignment

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:assignment_ui/page2.dart';
+import 'package:flutter/material.dart';
+//import 'package:news_ui/page2.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Page2(),
+    );
+  }
+}
